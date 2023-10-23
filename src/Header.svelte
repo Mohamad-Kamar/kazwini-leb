b9985ce2c5573ddc0cb8a3c03af15c08d7bcfc0a
mkamar <mohamad.kamar@agoda.com>
2023-10-23 20:52:00 +0700

    Update svelte and add header component

---
+++ src/Header.svelte
@@ -0,0 +1,66 @@
+<script>
+	// Add your script logic here if needed
+  </script>
+  
+  <!-- Header -->
+  <header>
+	<div class="container">
+	  <div class="row align-items-center">
+		<div class="col">
+		  <!-- Navbar Toggle Button -->
+		  <button type="button" class="btn btn-light" id="menu-toggle">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		  <!-- Navigation Menu -->
+		  <ul class="navbar-nav">
+			<li class="nav-item">
+			  <a class="nav-link" href="https://www.kazwini.com.br/pagina/meios-de-pagamento-e-de-frete.html">Meios de pagamento e de frete</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="https://www.kazwini.com.br/pagina/politica-de-privacidade.html">Política de privacidade</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="https://www.kazwini.com.br/pagina/politica-de-trocas-e-devolucoes.html">Política de Trocas e Devoluções</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="https://www.kazwini.com.br/pagina/quem-somos.html">Quem somos</a>
+			</li>
+		  </ul>
+		</div>
+		<div class="col-auto">
+		  <!-- Logo -->
+		  <h1 class="logo">
+			<a href="https://www.kazwini.com.br/" title="Kazwini Eyewear">
+			  <img src="https://cdn.awsli.com.br/400x300/2646/2646596/logo/grava-o-na-lente-1--2--8wjz778gh0.png" alt="Kazwini Eyewear" class="img-fluid">
+			</a>
+		  </h1>
+		</div>
+		<div class="col">
+		  <!-- Search Button -->
+		  <button type="button" class="btn btn-light" id="search-trigger">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+		  <!-- Account Dropdown -->
+		  <div class="dropdown">
+			<a class="btn btn-light dropdown-toggle" href="#" role="button" id="account-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			  My Account
+			</a>
+			<div class="dropdown-menu" aria-labelledby="account-dropdown">
+			  <a class="dropdown-item" href="/conta/login">Login</a>
+			  <a class="dropdown-item" href="/conta/login">Register</a>
+			  <a class="dropdown-item" href="/conta/index">My Account</a>
+			  <a class="dropdown-item" href="/conta/pedido/listar">My Orders</a>
+			</div>
+		  </div>
+		  <!-- Cart Button -->
+		  <div class="carrinho">
+			<a href="https://www.kazwini.com.br/carrinho/index" class="btn btn-light">
+			  <span class="navbar-toggler-icon"></span>
+			</a>
+			<strong class="qtd-carrinho titulo cor-secundaria" style="display: none;">0</strong>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </header>
+  
